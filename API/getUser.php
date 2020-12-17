846fd3d1c9367cc2c3b1f9fe5e1cfeb9bfe5edd4
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2020-12-18 01:41:54 +0700

    Migrasi ke codeigniter rest api

--- API/getUser.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(-1);
-ini_set('display_errors', 'On');
-date_default_timezone_set('Asia/Jakarta');
-
-require_once 'DbHandler.php';
-
-$db = new DbHandler();
-$db->tampilUser();
